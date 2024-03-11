52d11e732245a30bc0f7d15d89ed5ff968791775
Tristan Gérault <tristan.gerault@inrae.fr>
2024-03-11 09:04:22 +0100

    functionnal version until C_hexose_root_explodes related to diffusion

--- test/test_apply_scenario.py
+++ test/test_apply_scenario.py
@@ -17,7 +17,7 @@ def single_run(scenario, outputs_dirpath="test/outputs"):
                     recording_performance=True,
                     echo=True)
     
-    for step in range(300):
+    for step in range(250):
         # Placed here also to capture mtg initialization
         logger()
 
